c3f44ead4549342ef0c2c0b344c495727b7da665
Ishara Attanayake <chanuka16229@gmail.com>
2026-05-20 07:50:44 +0530

    Create pir.cpp

---
+++ src/pir.cpp
@@ -0,0 +1,50 @@
+#define F_CPU 16000000UL
+
+#include <Arduino.h>      
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    init();
+
+
+    DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2);
+
+   
+    DDRD = 0x00;
+
+  
+    PORTB = 0x00;
+
+    while (1)
+    {
+        
+        PORTB |= (1 << PB1);
+        _delay_ms(1000);
+
+        PORTB &= ~(1 << PB1);
+        _delay_ms(1000);
+
+       
+        if (PIND & (1 << PD2))
+        {
+            
+            PORTB |= (1 << PB0);
+
+            
+            tone(10, 1000);
+
+            _delay_ms(5000);
+
+       
+            noTone(10);
+        }
+        else
+        {
+            PORTB &= ~(1 << PB0);
+
+            noTone(10);
+        }
+    }
+}
